b50c51694c169bce5768dee13f2df756bae8fa4a
Denis Kuznetsov <detrevile3@gmail.com>
2022-03-06 19:18:55 +0300

    feat: add regexp

--- src/js/main.js
+++ src/js/main.js
@@ -384,11 +384,15 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesField.style.transform = `translateX(-${offset}px)`;
     }
 
+    function deleteLetters(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        if (offset == deleteLetters(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteLetters(width);
         }
 
         moveSlides();
@@ -405,9 +409,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteLetters(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteLetters(width);
         }
 
         moveSlides();
@@ -427,7 +431,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+            offset = deleteLetters(width) * (slideTo - 1)
 
             moveSlides();
             slidesCheck();
